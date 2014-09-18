9d08020039dc4def0389a1f7462836fb4395099b
Wing Gao <wing.gao@live.com>
2014-09-18 10:54:19 +0800

    change to class

--- clip_osx.py
+++ clip_osx.py
@@ -4,39 +4,72 @@ import platform
 
 __author__ = 'wing'
 
+CL_TEXT = 0
+CL_IMAGE = 1
+
+
+class ClipContentItem():
+    def __init__(self):
+        self.cl_type = None
+        self.cl_data = None
+
+
+class Clipboard(object):
+    def __init__(self):
+        pass
+
+    def paste(self):
+        raise Exception('no method')
+
+
+class Clipboard_Win(Clipboard):
+    def __init__(self):
+        super(Clipboard_Win, self).__init__()
+
+    def paste(self):
+        pass
 
 def write_to_file(data):
     with open('a.png', 'wb') as f:
         f.write(data)
-#osx
-def _get_pb_osx():
-    return NSPasteboard.generalPasteboard()
 
-def _check_image_osx(pb):
-    pbImage = NSImage.alloc().initWithPasteboard_(pb)
-    if pbImage is not None:
-        pngData = NSBitmapImageRep.representationOfImageRepsInArray_usingType_properties_(
-            pbImage.representations(), NSPNGFileType, None)
-        return pngData
 
-def _check_text_osx(pb):
-    pbString = pb.stringForType_(NSStringPboardType)
-    return pbString.encode('utf8')
+def _check_image_win():
+    ctypes.windll.user32.OpenClipboard(0)  # 8 is CF_DIB
 
-def loop():
-    img = check_image(MyPb)
-    if img is not None:
-        print img
-    else:
-        text = check_text(MyPb)
-        print text
+    pass
+
+
+class Clipboard_OSX(Clipboard):
+    def __init__(self):
+        super(Clipboard_OSX, self).__init__()
+        self.pb = NSPasteboard.generalPasteboard()
+
+    def paste(self):
+        img = self._check_image_osx()
+        if img is not None:
+            print img
+        else:
+            text = self._check_text_osx()
+            print text
+
+    def _check_image_osx(self):
+        pbImage = NSImage.alloc().initWithPasteboard_(self.pb)
+        if pbImage is not None:
+            pngData = NSBitmapImageRep.representationOfImageRepsInArray_usingType_properties_(
+                pbImage.representations(), NSPNGFileType, None)
+            return pngData
+
+
+    def _check_text_osx(self):
+        pbString = self.pb.stringForType_(NSStringPboardType)
+        return pbString.encode('utf8')
+
 
 if os.name == 'nt' or platform.system() == 'Windows':
+    import ctypes
+
     pass
 elif os.name == 'mac' or platform.system() == 'Darwin':
     from AppKit import *
-    MyPb = _get_pb_osx()
-    check_image = _check_image_osx
-    check_text = _check_text_osx
 
-loop()
